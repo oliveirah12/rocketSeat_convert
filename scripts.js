88d9ae721e0212f23c1ed46f7a4103add4126edd
Matheus_Oliveira_Cne <matheus@cnesistemas.com.br>
2025-04-29 17:41:37 -0300

    conversion and formatting currency

---
+++ scripts.js
@@ -0,0 +1,61 @@
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const currency = document.getElementById("currency");
+const footer = document.querySelector("main footer")
+const description = document.getElementById("description")
+const result = document.getElementById("result")
+
+const cotacao = {
+  "USD" : 4.87,
+  "EUR" : 5.30,
+  "GBP" : 6.87
+}
+
+
+amount.oninput = () => {
+  const hasCharactersRegex = /\D+/g
+  amount.value = amount.value.replace(hasCharactersRegex, "")
+}
+
+
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  switch(currency.value){
+    case "USD":
+      convertCurrency(amount.value, cotacao.USD, "US$")
+    break
+    case "EUR":
+      convertCurrency(amount.value, cotacao.EUR, "€$")
+    break
+    case "GBP":
+      convertCurrency(amount.value, cotacao.GBP, "£$")
+    break
+  }
+}
+
+
+function convertCurrency(amount, price, symbol) {
+  try{
+    let formattedResult =  formatCurrencyBRL(amount * price)
+    formattedPrice = formatCurrencyBRL(price)
+
+    description.textContent = `${symbol} 1 = ${formattedPrice}`
+    result.textContent = `${formattedResult}`
+
+    footer.classList.add("show-result")
+  }catch(error){
+    footer.classList.remove("show-result")
+    console.log(error)
+  }
+}
+
+
+function formatCurrencyBRL(value){
+  return Number(value).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL"
+  })
+}
+
+
